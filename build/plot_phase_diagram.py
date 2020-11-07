84b23e064a1dc88350fe99b36c687d06502a83dd
H1dek1 <hidekitakayama19960921@gmail.com>
2020-11-07 14:22:02 +0900

    commit

--- build/plot_phase_diagram.py
+++ build/plot_phase_diagram.py
@@ -19,15 +19,15 @@ ax.set_yscale('log')
 #ax.set_ylim(0, 200)
 cm = plt.cm.get_cmap('Blues')
 #-------------------
-data = np.loadtxt('../phases/phases_mtg0.01.txt')
+data = np.loadtxt('../phases/phases_gamma0.01.txt')
 alpha = data[:,0]
 beta = data[:,1]
 gamma = data[:,2]
 
 mn = 1. / (beta*(0.3**3))
 trf = mn / alpha
-print(mn.shape)
-print(trf.shape)
+#print(mn.shape)
+#print(trf.shape)
 
 n_iter = data[:,3]
 pos_x = data[:,4]
@@ -41,9 +41,9 @@ print(max(pos_y))
 #    else:
 #        mappable = ax.scatter(alpha[i], beta[i], c=pos_y[i], vmin=min(pos_y)-0.2, vmax=max(pos_y)+0.0, s=35, cmap=cm)
 
-mappable = ax.scatter(mn, trf, c=pos_y, vmin=min(pos_y)-0.2, vmax=max(pos_y)+0.2, s=35, cmap=cm)
+mappable = ax.scatter(alpha, beta, c=pos_y, vmin=min(pos_y)-0.2, vmax=max(pos_y)+0.2, s=35, cmap=cm)
 fig.colorbar(mappable, ax=ax)
 plt.subplots_adjust(left=0.12, right=1.0, bottom=0.1, top=0.9)
-fig.savefig('../phases/phase_mtg0.01.eps')
-fig.savefig('../phases/phase_mtg0.01.png')
+fig.savefig('../phases/phase_gamma0.01.eps')
+fig.savefig('../phases/phase_gamma0.01.png')
 plt.show()
