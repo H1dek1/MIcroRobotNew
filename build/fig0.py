10d96de7694e02ced33ac73a412a02821e773b30
H1dek1 <hidekitakayama19960921@gmail.com>
2021-01-09 17:47:45 +0900

    commit

--- build/fig0.py
+++ build/fig0.py
@@ -80,7 +80,7 @@ ext_v = 0.8*np.sin(np.pi/6)
 ax0.plot([ext_x, ext_x+1.0], [ext_y, ext_y], linestyle='--', color='k', lw=1, zorder=5)
 ax0.plot([ext_x, ext_x+1.0*np.cos(np.pi/6)], [ext_y, ext_y+1.0*np.sin(np.pi/6)], linestyle='--', color='k', lw=1, zorder=5)
 #ax0.quiver(ext_x, ext_y, ext_u, ext_v, pivot='mid', scale=5, width=1.0e-2, headwidth=3, headlength=5, headaxislength=4, color='black', zorder=2)
-arrow_dict4 = dict(arrowstyle = '<->, head_width=0.15, head_length=0.3', color='k', linestyle='-', shrinkA=0, shrinkB=0, lw=2.0e+0)
+arrow_dict4 = dict(arrowstyle = '<->, head_width=0.10, head_length=0.4', color='k', linestyle='-', shrinkA=0, shrinkB=0, lw=2.0e+0)
 ax0.annotate('',
         xy=(ext_x+ext_u/2, ext_y+ext_v/2),
         xytext=(ext_x-ext_u/2, ext_y-ext_v/2),
@@ -152,7 +152,7 @@ ax0.text(1.05, 0-0.3, r'$\left(\cos\theta_2, \sin\theta_2 \right)$', fontsize=15
 """
 ax1
 """
-LX = 2.0
+LX = 1.60
 LY = 1.0
 gridwidth = 0.1
 ratio = 1.0
@@ -166,11 +166,28 @@ ax1.set_aspect('equal')
 
 ax1.set_yticks([-1, 0, 1])
 pos_1 = [ratio * -0.5, 0]
-pos_2 = [ratio * 0.5, 0]
+pos_2 = [ratio * 0.5, 0 ]
 perm1 = patches.Circle(xy=(pos_1[0], pos_1[1]), radius=ratio * 0.3, ls='-', ec='gray', fc='gray', fill=True)
 perm2 = patches.Circle(xy=(pos_2[0], pos_2[1]), radius=ratio * 0.3, ls='-', ec='gray', fc='gray', fill=True)
 ax1.add_patch(perm1)
 ax1.add_patch(perm2)
+left_arc  = patches.Arc(xy=(ratio * -0.5, 0), lw=4.0, width=0.40, height=0.40, angle=0.0, theta1=-240, theta2=60)
+right_arc = patches.Arc(xy=(ratio * 0.5, 0 ), lw=4.0, width=0.40, height=0.40, angle=0.0, theta1=-240, theta2=60)
+ax1.add_patch(left_arc)
+ax1.add_patch(right_arc)
+arrow_dict = dict(arrowstyle = '->, head_width=0.3, head_length=0.5', color='k', linestyle='-', shrinkA=0, shrinkB=0, lw=3.0e+0)
+ax1.annotate('',
+        xy=(-0.5+0.2*np.cos(np.pi/3)+0.05*np.cos(np.pi/3+np.pi/2-np.pi/30), 0.2*np.sin(np.pi/3)+0.05*np.sin(np.pi/3+np.pi/2-np.pi/30)),
+        xytext=(-0.5+0.2*np.cos(np.pi/3), 0.2*np.sin(np.pi/3)),
+        arrowprops=arrow_dict,
+        color='k',
+        zorder=4)
+ax1.annotate('',
+        xy=(0.5+0.2*np.cos(2*np.pi/3)+0.05*np.cos(2*np.pi/3-np.pi/2+np.pi/30), 0.2*np.sin(2*np.pi/3)+0.05*np.sin(2*np.pi/3-np.pi/2+np.pi/30)),
+        xytext=(0.5+0.2*np.cos(2*np.pi/3), 0.2*np.sin(2*np.pi/3)),
+        arrowprops=arrow_dict,
+        color='k',
+        zorder=4)
 x_arr = np.arange(-LX, LX, gridwidth)
 y_arr = np.arange(-LY, LY, gridwidth)
 X, Y = np.meshgrid(x_arr, y_arr)
@@ -200,8 +217,8 @@ for i in range(len(y_arr)):
 
 
 ax1.quiver(X, Y, U, V, color='C0', angles='xy', scale_units='xy', scale=1.0)
-ax1.quiver(pos_1[0], pos_1[1], 0., 0.8, color='red', angles='xy', scale_units='xy', scale=1.0, zorder=2)
-ax1.quiver(pos_2[0], pos_2[1], 0., 0.8, color='red', angles='xy', scale_units='xy', scale=1.0, zorder=2)
+ax1.quiver(pos_1[0], pos_1[1], 0., 0.8, color='red', angles='xy', scale_units='xy', scale=1.0, zorder=2, headwidth=4, headlength=6, headaxislength=5)
+ax1.quiver(pos_2[0], pos_2[1], 0., 0.8, color='red', angles='xy', scale_units='xy', scale=1.0, zorder=2, headwidth=4, headlength=6, headaxislength=5)
 
 
 """
@@ -218,38 +235,62 @@ ax2.axis('off')
 """
 ax3
 """
-width = 535
-height = 332
+width = 858
+height = 460
 origin_x = width/2
 origin_y = height/2
-ax3.set_xlim(origin_x-400, origin_x+400)
-ax3.set_ylim(origin_y+200, origin_y-250)
+ax3.set_xlim(origin_x-430, origin_x+430)
+ax3.set_ylim(origin_y+250, origin_y-300)
 ax3.set_aspect('equal')
-img3 = plt.imread('chlamidomonas2.jpg')
+img3 = plt.imread('chlamidomonas3.jpg')
 ax3.imshow(img3, zorder=0)
 ax3.axis('off')
 
 center_x = origin_x
 center_y = origin_y + 80
 body = patches.Circle(xy=(center_x, center_y), radius=40, fc='black', fill=True, zorder=1)
-left_hand  = patches.Circle(xy=(center_x-200, 10), radius=15, fc='black', fill=True, zorder=1)
-right_hand = patches.Circle(xy=(center_x+200, 10), radius=15, fc='black', fill=True, zorder=1)
+left_hand  = patches.Circle(xy=(center_x-320, 13), radius=15, fc='black', fill=True, zorder=1)
+right_hand = patches.Circle(xy=(center_x+320, 13), radius=15, fc='black', fill=True, zorder=1)
 
-RADI = 80
+RADI = 100
+beam = 340
+ang = np.pi/4.5
 circle_x = 110 + RADI*np.cos(3*np.pi/8)
 circle_y = 6   + RADI*np.sin(3*np.pi/8)
 #left_circle  = patches.Circle(xy=(origin_x-circle_x, circle_y), radius=RADI, ec='black', fill=False, zorder=1)
 #right_circle = patches.Circle(xy=(origin_x+circle_x, circle_y), radius=RADI, ec='black', fill=False, zorder=1)
-left_circle  = patches.Circle(xy=(center_x-250*np.cos(np.pi/3), center_y-250*np.sin(np.pi/3)), radius=RADI, ec='black', fill=False, zorder=1)
-right_circle = patches.Circle(xy=(center_x+250*np.cos(np.pi/3), center_y-250*np.sin(np.pi/3)), radius=RADI, ec='black', fill=False, zorder=1)
+left_circle  = patches.Circle(xy=(center_x-beam*np.cos(ang), center_y-beam*np.sin(ang)), radius=RADI, ec='black', fill=False, zorder=1)
+right_circle = patches.Circle(xy=(center_x+beam*np.cos(ang), center_y-beam*np.sin(ang)), radius=RADI, ec='black', fill=False, zorder=1)
 ax3.add_patch(body)
 ax3.add_patch(left_hand)
 ax3.add_patch(right_hand)
 ax3.add_patch(left_circle)
 ax3.add_patch(right_circle)
-ax3.plot([center_x-250*np.cos(np.pi/3), center_x+250*np.cos(np.pi/3)], [center_y-250*np.sin(np.pi/3), center_y-250*np.sin(np.pi/3)], lw=1.5, color='black')
-ax3.plot([center_x-250*np.cos(np.pi/3), center_x                    ], [center_y-250*np.sin(np.pi/3), center_y                    ], lw=1.5, color='black')
-ax3.plot([center_x                    , center_x+250*np.cos(np.pi/3)], [center_y                    , center_y-250*np.sin(np.pi/3)], lw=1.5, color='black')
+ax3.plot([center_x-beam*np.cos(ang), center_x+beam*np.cos(ang)], [center_y-beam*np.sin(ang), center_y-beam*np.sin(ang)], lw=1.5, color='black')
+ax3.plot([center_x-beam*np.cos(ang), center_x                 ], [center_y-beam*np.sin(ang), center_y                 ], lw=1.5, color='black')
+ax3.plot([center_x                 , center_x+beam*np.cos(ang)], [center_y                 , center_y-beam*np.sin(ang)], lw=1.5, color='black')
+
+# arrows
+left_arc  = patches.Arc(xy=(center_x-beam*np.cos(ang),center_y-beam*np.sin(ang)), lw=3.0, width=300, height=300, angle=0.0, theta1=135, theta2=225)
+right_arc = patches.Arc(xy=(center_x+beam*np.cos(ang),center_y-beam*np.sin(ang)), lw=3.0, width=300, height=300, angle=0.0, theta1=-45, theta2=45)
+ax3.add_patch(left_arc)
+ax3.add_patch(right_arc)
+#ax3.quiver(center_x-beam*np.cos(ang)+300*np.cos(5*np.pi/4), center_y-beam*np.sin(ang)+300*np.sin(5*np.pi/4), 400, -400, color='red', angles='xy', scale_units='xy', scale=1.0, zorder=5)
+#ax3.quiver(center_x-beam*np.cos(ang)+150*np.cos(5*np.pi/4), center_y-beam*np.sin(ang)-150*np.sin(5*np.pi/4), 10, 10, headlength=100, headaxislength=90, headwidth=60, color='k', angles='xy', scale_units='xy', scale=0.5, zorder=5)
+arrow_dict = dict(arrowstyle = '->, head_width=0.2, head_length=0.5', color='k', linestyle='-', shrinkA=0, shrinkB=0, lw=2.0e+0)
+ax3.annotate('',
+        xy=(center_x-beam*np.cos(ang)+150*np.cos(5*np.pi/4)+10, center_y-beam*np.sin(ang)-150*np.sin(5*np.pi/4)+10),
+        xytext=(center_x-beam*np.cos(ang)+150*np.cos(5*np.pi/4), center_y-beam*np.sin(ang)-150*np.sin(5*np.pi/4)),
+        arrowprops=arrow_dict,
+        color='k',
+        zorder=4)
+ax3.annotate('',
+        xy=(center_x+beam*np.cos(ang)+150*np.cos(np.pi/4)-10, center_y-beam*np.sin(ang)+150*np.sin(np.pi/4)+10),
+        xytext=(center_x+beam*np.cos(ang)+150*np.cos(np.pi/4), center_y-beam*np.sin(ang)+150*np.sin(np.pi/4)),
+        arrowprops=arrow_dict,
+        color='k',
+        zorder=4)
+#ax1.quiver(pos_2[0], pos_2[1], 0., 0.8, color='red', angles='xy', scale_units='xy', scale=1.0, zorder=2)
 
 plt.show()
 fig.savefig('papers/figure_0.png')
